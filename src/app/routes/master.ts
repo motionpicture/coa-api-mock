918bf0df7661c52392f3635a17aff77929c4d877
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-12-05 04:15:48 +0000

    #SSKTS-714 ### Added - マスター抽出ルーターを追加。

--- src/app/routes/master.ts
+++ src/app/routes/master.ts
@@ -4,11 +4,60 @@
  */
 
 import { Router } from 'express';
-// import { OK, TOO_MANY_REQUESTS } from 'http-status';
-// import * as querystring from 'querystring';
-
-// import validator from '../middlewares/validator';
+import * as fs from 'fs';
 
 const masterRouter = Router();
+
+masterRouter.get(
+    '/theater/:theaterCode/theater/',
+    (__, res) => {
+        fs.createReadStream(`${__dirname}../../../data/theater`).pipe(res);
+    });
+
+masterRouter.get(
+    '/theater/:theaterCode/title/',
+    (__, res) => {
+        fs.createReadStream(`${__dirname}../../../data/title`).pipe(res);
+    });
+
+masterRouter.get(
+    '/theater/:theaterCode/screen/',
+    (__, res) => {
+        fs.createReadStream(`${__dirname}../../../data/screen`).pipe(res);
+    });
+
+masterRouter.get(
+    '/theater/:theaterCode/schedule/',
+    (__, res) => {
+        fs.createReadStream(`${__dirname}../../../data/schedule`).pipe(res);
+    });
+
+masterRouter.get(
+    '/theater/:theaterCode/ticket/',
+    (__, res) => {
+        fs.createReadStream(`${__dirname}../../../data/ticket`).pipe(res);
+    }
+);
+
+masterRouter.get(
+    '/theater/:theaterCode/kubun_name/',
+    (__, res) => {
+        fs.createReadStream(`${__dirname}../../../data/kubunName`).pipe(res);
+    });
+
+masterRouter.get(
+    '/theater/:theaterCode/mvtk_ticketcode/',
+    (__, res) => {
+        res.json({
+            status: 0,
+            message: '',
+            ticket_code: '',
+            ticket_name: '',
+            ticket_name_kana: '',
+            ticket_name_eng: '',
+            add_price: 100,
+            add_price_glasses: 100
+        });
+    });
 
 export default masterRouter;
